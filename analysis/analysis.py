3cc4eb2cece86cb43ed7751038dc518645adc169
johnwashburne <johnwashburne@outlook.com>
2021-11-21 18:44:28 -0500

    Distribution graph

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -3,11 +3,22 @@ import csv
 import matplotlib.pyplot as plt
 
 gpas = []
-with open('results.csv', 'r') as csvfile:
+table = []
+with open('results_sorted.csv', 'r') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         gpas.append(float(row[1]))
+        table.append([row[0], row[1]])
 
 x = np.array(gpas)
 plt.hist(x, histtype='stepfilled', color='steelblue', edgecolor=None, bins=20, alpha=.50)
 plt.show()
+
+print("Major Program | Average GPA")
+print("---- | ----")
+
+for row in table:
+    program = row[0]
+    program = program.replace("Bachelor of Science in ", "")
+    program = program.replace("Bachelor of", "")
+    print(program, "|", round(float(row[1]), 3))
